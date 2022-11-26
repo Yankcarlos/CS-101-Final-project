454dc24dcc6c1a1d7565e117484bf6f2e2d33883
yankcarlos padron <padronyankcarlos@gmail.com>
2022-11-25 22:34:22 -0500

    clean up and bug fixing what i broke

--- Battleships.py
+++ Battleships.py
@@ -1,19 +1,16 @@
-#to ge the random numbers
 import random
 #to clear the terminal  
 import os
-#count for switching players
-count = 0
 class board:
   def __init__(self):
-   self.hits = 0
+   self.hits = 16
    self.scount = 0
    self.board = []
-   self.rows = []
-   self.cols = []
+   self.rows_cols = {'row':[],'col':[]}
    self.win = False
-   
-  #making the boards   
+   self.create_board()
+  count = 0
+
   def create_board(self):
    cords_C = [' A',' B',' C',' D',' E',' F',' G',' H',' I',' J']
    for i in range(11):
@@ -26,36 +23,20 @@ class board:
      ns = str(n)
      self.board[n][0] = ns + ' '
      self.board[10][0] = '10'
-  #showing the boards      
+         
   def show_board(self):
       for row in self.board:
           for item in row:
               print(item, end=" ")
           print()
-          
-  #placing the ships            
+              
   def place_ships(self,ship,row,col,dir):
-   count1 = 0
-   count2 = 0 
-   if ship == "Carrier":
-    pass
-   elif ship == "Battleship":
-    count1 = 1
-    count2 = 1
-   elif ship == "Destroyer":
-    count1 = 2
-    count2 = 2
-   elif ship == "Submarine": 
-    count1 = 2
-    count2 = 2
-   elif ship == "Patrol Boat":
-    count1 = 3
-    count2 = 3
+   count = {"Carrier":0,"Battleship":1,"Destroyer":2,"Submarine":2,"Patrol boat":3,}
    #place ships horizontaly and make sure ships won't overlap  
    if dir == 'H':  
     try:
-        while count1 < 5:
-         count1 += 1 
+        while count[ship] < 5:
+         count[ship] += 1
          while col < 1:
            col += 1
          while self.board[row][col] == '🟩':
@@ -64,8 +45,8 @@ class board:
           self.board[row][col] = '🟩'
           col -= 1
     except:
-         while count2 < 5:
-          count2 += 1
+         while count[ship] < 5:
+          count[ship] += 1
           while col > 10:
             col -= 1
           while self.board[row][col] == '🟩':
@@ -76,8 +57,8 @@ class board:
     #place ships virtacly and make sure ships wont overlap
    elif dir == 'V': 
     try:
-     while count1 < 5:
-        count1 += 1 
+     while count[ship]  < 5:
+        count[ship] += 1
         while row < 1:
           row += 1
         while self.board[row][col] == '🟩':
@@ -86,8 +67,8 @@ class board:
           self.board[row][col] = '🟩'
           row -= 1
     except:
-        while count2 < 5:
-         count2 += 1
+        while count[ship] < 5:
+         count[ship] += 1
          while row > 10:
           row -= 1
          while self.board[row][col] == '🟩':
@@ -102,30 +83,26 @@ class board:
    col = random.randint(1,10) 
    rdir = random.randint(1,2)
    if rdir == 1:
-     dir = 'V'
-   else:
      dir = 'H'
-  #only blocks rows for horizantal placements 
-   if dir == 'H':
-    self.rows.append(row)  
-  #only blocks coloms for vertical placements 
-   else: 
-    self.cols.append(col)
-  #checks for duplicates in the list of row and col placements and makes sure there arent any
-   myset = set(self.rows)  
-   mysetc = set(self.cols)
-   if len(self.rows) == len(myset) and dir == 'H' or len(self.cols) == len(mysetc) and dir == 'V':  
-    cp.place_ships(ships,row,col,dir)
-    board_copy.place_ships(ships,row,col,dir)  
+     #only blocks rows for horizantal placements 
+     self.rows_cols['row'].append(row)
    else:
-    #removes the duplicate so that the code countinues to run
+     dir = 'V'
+     #only blocks coloms for vertical placements 
+     self.rows_cols['col'].append(col)  
+  #checks for duplicates in the list of row and col placements and makes sure there arent any
+   myset = {"rows":set(self.rows_cols['row']),"cols":set(self.rows_cols['col'])}
+   if len(self.rows_cols['row']) == len(myset['rows']) and dir == 'H' or len(self.rows_cols['col']) == len(myset['cols']) and dir == 'V':  
+    cp.place_ships(ships,row,col,dir)
+    cp_board_copy.place_ships(ships,row,col,dir)  
+   else:
+    #removes the duplicate and calls the function again so that the code countinues to run
      try: 
-      self.rows.remove(row)
-      self.cols.remove(col)
+      self.rows_cols['row'],['col'].pop(row,col)
      except:
       self.cp_place() 
              
-  #the code to attack and check if you have or havent hit that spot already and counts the hits from the player and computer
+  #checks if you have or havent hit that spot already and counts the hits from the player and computer
   def attack(self,row,col):
    if self == player_1 and cp.board[row][col] == '🟥' or cp.board[row][col] == '⬜':
        print('You already hit that spot') 
@@ -136,12 +113,12 @@ class board:
     if self == player_1:
      if cp.board[row][col] == '🟩':
        cp.board[row][col] = '🟥'
-       board_copy.board[row][col] = '🟥'
+       cp_board_copy.board[row][col] = '🟥'
        self.hits += 1
        self.switch()
      elif cp.board[row][col]  == '🟦':
        cp.board[row][col] = '⬜'
-       board_copy.board[row][col] = '⬜'
+       cp_board_copy.board[row][col] = '⬜'
        self.switch()
     else:
       if player_1.board[row][col] == '🟩':
@@ -152,7 +129,7 @@ class board:
        player_1.board[row][col] = '⬜'
        self.switch()
    
-  #Hides the enemys ships on the copyed board for attacking
+  #Hides the enemys ships on the fake board 
   def hide(self):
    crow = -1
    for row in self.board:
@@ -167,12 +144,10 @@ class board:
    print('Enemy board')
    self.show_board()
         
-  #win conditions
   def wins(self):
    if self.hits == 17:
     self.win = True
   
-  #changes the ships when placing them  
   def changing_ships(self):
    global ships
    self.scount += 1
@@ -187,45 +162,40 @@ class board:
    else:
       ships = "Patrol boat"
   
-  #switchs turns
   def switch(self): 
-   global count 
-   count += 1
+   board.count += 1
    if self == cp:
      self = player_1
    else:
      self = cp
-   if count == 1:
+   if board.count == 1:
      self.place_ship()
    else:
      self.loop()
-      
-  #starts game and calls places ships function as well as creating the players board    
+     
   def start(self):
-    os.system('cls')
-    print('''        
- B B B B        A       T T T T T   T T T T T   L          E E E E E     S S S S   H       H    I    P P P P     S S S S 
-B       B     A   A         T           T       L          E           S           H       H    I    P       P  S
-B       B   A       A       T           T       L          E           S           H       H    I    P       P  S
-B B B B     A A A A A       T           T       L          E E E         S S S     H H H H H    I    P P P P      S S S
-B       B   A       A       T           T       L          E                   S   H       H    I    P                  S
-B       B   A       A       T           T       L          E                   S   H       H    I    P                  S
-B B B B     A       A       T           T       L L L L L  E E E E E   S S S S     H       H    I    P          S S S S 
-''')
-    s = input('Type Y to start or N to cancel: ') 
-    s = s.upper()
-    if s == 'Y':
-     self.create_board()
-     self.place_ship()
-    elif s == 'N':
-      pass
-    else:
-      print('You didnt type one of the options')
-      self.start()
-  
-  #function for placing ships
+      os.system('cls')
+      print('''        
+   B B B B        A       T T T T T   T T T T T   L          E E E E E     S S S S   H       H    I    P P P P     S S S S 
+  B       B     A   A         T           T       L          E           S           H       H    I    P       P  S
+  B       B   A       A       T           T       L          E           S           H       H    I    P       P  S
+  B B B B     A A A A A       T           T       L          E E E         S S S     H H H H H    I    P P P P      S S S
+  B       B   A       A       T           T       L          E                   S   H       H    I    P                  S
+  B       B   A       A       T           T       L          E                   S   H       H    I    P                  S
+  B B B B     A       A       T           T       L L L L L  E E E E E   S S S S     H       H    I    P          S S S S 
+  ''')
+      s = input('Type Y to start or N to cancel: ') 
+      s = s.upper()
+      if s == 'Y':
+       self.place_ship()
+      elif s == 'N':
+        pass
+      else:
+        print('You didnt type one of the options')
+        self.start()
+        
   def place_ship(self):
-   cords = ['A','B','C','D','E','F','G','H','I','J']
+   cords = {'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9,'J':10}
    if self == player_1:
     for s in range(5):
       self.changing_ships()
@@ -254,8 +224,7 @@ B B B B     A       A       T           T       L L L L L  E E E E E   S S S S  
             Cchoice = input('What colom would you like to place ' + ships + " A-J: ")
             Cchoice = Cchoice.upper()
           else:
-           n = cords.index(Cchoice)
-           col = n + 1
+           col = cords[Cchoice]
         #dir choice
            dir = input('What direction would you like to place the ship horizontally or vertically type V or H: ')
            dir = dir.upper()
@@ -266,27 +235,21 @@ B B B B     A       A       T           T       L L L L L  E E E E E   S S S S  
            else:
             self.place_ships(ships,row,col,dir)
     self.switch()
-   #creates the main board and copy of that board for the computer and starts the loop for the computer to place its ships
+   #starts the loop for the computer to place its ships
    else:
-    self.create_board()
-    board_copy.create_board()
     for sc in range(5):
      self.changing_ships()
      self.cp_place()
     self.switch()
-      
-  #main game loop    
+       
   def loop(self):
-   cords = ['A','B','C','D','E','F','G','H','I','J']
+   cords = {'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9,'J':10}
    self.wins()
    if self == player_1 and player_1.win == False:
     os.system('cls')
-    print(self.hits)
     print('Your board')
     player_1.show_board()
-    cp.show_board()
-    board_copy.hide()
-    #row choice
+    cp_board_copy.hide()
     row = input('What row would you like to attack the 1-10: ')
     while row.isdigit() == False:
       print('You didnt type a number between 1 and 10')
@@ -301,7 +264,6 @@ B B B B     A       A       T           T       L L L L L  E E E E E   S S S S  
        else:
         row = int(row)
       else:
-      #col choice
         Cchoice = input('What colom would you like to attack A-J: ')
         Cchoice = Cchoice.upper()
         while Cchoice not in cords:
@@ -309,8 +271,7 @@ B B B B     A       A       T           T       L L L L L  E E E E E   S S S S  
           Cchoice = input('What colom would you like to attack A-J: ')
           Cchoice = Cchoice.upper()
         else:
-         n = cords.index(Cchoice)
-         col = n + 1
+         col = cords[Cchoice]
          self.attack(row,col)
     #The computer for attacking very simple could be improved 
    elif self == cp and cp.win == False :
@@ -326,19 +287,11 @@ B B B B     A       A       T           T       L L L L L  E E E E E   S S S S  
       print('enemy board')
       cp.show_board()
       #play again option
-      s = input('Do you want play agian type Y or N: ')
-      s = s.upper()
-      if s == 'Y':
-      #Clearing everything from previous game
-       cp.board =[]
-       player_1.board = []
-       board_copy.board = []
-       cp.hits = 0
-       player_1.hits = 0
-       player_1.scount = 0
-       cp.scount = 0
-       self.start()
-      elif s == 'N':
+      rs = input('Do you want play agian type Y or N: ')
+      rs = rs.upper()
+      if rs == 'Y':
+       player_1.restart()
+      elif rs == 'N':
         pass
      else:
       os.system('cls')
@@ -348,22 +301,27 @@ B B B B     A       A       T           T       L L L L L  E E E E E   S S S S  
       print('Enemy board')
       cp.show_board()
       #play again option
-      s = input('Do you want try agian type Y or N: ')
-      s = s.upper()
-      if s == 'Y':
-      #Clearing everything from previous game
-       cp.board =[]
-       player_1.board = []
-       board_copy.board = []
-       cp.hits = 0
-       player_1.hits = 0
-       player_1.scount = 0
-       cp.scount = 0
-       self.start()
-      elif s == 'N':
+      rs = input('Do you want try agian type Y or N: ')
+      rs = rs.upper()
+      if rs == 'Y':
+       player_1.restart()
+      elif rs == 'N':
         pass
+      
+  #Clearing everything from previous game    
+  def restart(self):
+    for i in range(2):
+     self.board =[]
+     cp_board_copy.board = []
+     self.hits = 0
+     self.scount = 0
+     self.rows_cols = {'row':[],'col':[]}
+     self.create_board()
+     self == cp
+    cp_board_copy.create_board()
+    player_1.start()
     
 player_1 = board()
 cp = board()
-board_copy = board()
+cp_board_copy = board()
 player_1.start()
